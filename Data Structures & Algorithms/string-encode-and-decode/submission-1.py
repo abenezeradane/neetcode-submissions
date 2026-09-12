e0c9a9c8e7414489e344d85fa069d63995cf810c
Abenezer Adane <32374905+abenezeradane@users.noreply.github.com>
2026-09-12 18:25:03 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for string in strs:
+            encoded += string + "%0"
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        decoded = s.split("%0")
+        return decoded[:len(decoded) - 1]
