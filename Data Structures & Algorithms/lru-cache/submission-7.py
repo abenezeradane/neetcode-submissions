80c1bc74417a87fb9963059419237f7e64f41651
Abenezer Adane <32374905+abenezeradane@users.noreply.github.com>
2026-09-24 20:01:46 -0700

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = dict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        value = self.cache.pop(key)
+        self.cache[key] = value
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+
+        if len(self.cache) == self.capacity:
+            lru = list(self.cache.keys())[0]
+            self.cache.pop(lru)
+
+        self.cache[key] = value
